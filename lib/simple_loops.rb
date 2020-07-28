124b934dfa23e1a6d09f7ffeee987f2a82c98f04
Milan Winter <milanwinter8@gmail.com>
2020-07-28 23:05:54 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -22,3 +22,12 @@ def output_array array
     i += 1
   end
 end
+
+def return_string_array array
+  i = 0
+  while i < array.length do 
+    new_array.push(array[i].to_s)
+    i += 1
+  end
+  new_array
+end
